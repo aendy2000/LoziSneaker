777d7dbc16e6fd4dc9e8a14b445dbe9859ba4496
DANGTHIMINHNGOC <ngoc.197pm21959@vanlanguni.vn>
2021-05-31 12:03:38 +0700

    Add management bag

---
+++ WebApplication1/WebApplication1/Controllers/CHITIETGIOHANGsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CHITIETGIOHANGsController : Controller
+    {
+        private CT25Team13Entities db = new CT25Team13Entities();
+
+        // GET: CHITIETGIOHANGs
+        public ActionResult Index(string id)
+        {
+            var cHITIETGIOHANGs = db.CHITIETGIOHANGs.Where(c => c.MAGIOHG == id).ToList();
+            return View(cHITIETGIOHANGs.ToList());
+        }
+
+        // GET: CHITIETGIOHANGs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CHITIETGIOHANG cHITIETGIOHANG = db.CHITIETGIOHANGs.Find(id);
+            if (cHITIETGIOHANG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cHITIETGIOHANG);
+        }
+
+        // POST: CHITIETGIOHANGs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CHITIETGIOHANG cHITIETGIOHANG = db.CHITIETGIOHANGs.Find(id);
+            db.CHITIETGIOHANGs.Remove(cHITIETGIOHANG);
+            db.SaveChanges();
+            return RedirectToAction("Index", "GIOHANGs");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/GIOHANGsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class GIOHANGsController : Controller
+    {
+        private CT25Team13Entities db = new CT25Team13Entities();
+
+        // GET: GIOHANGs
+        public ActionResult Index()
+        {
+            var gIOHANGs = db.GIOHANGs.Include(g => g.AspNetUser);
+            return View(gIOHANGs.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
