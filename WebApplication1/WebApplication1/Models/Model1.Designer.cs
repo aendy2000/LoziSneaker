c91754a55866741100d296b39e34d1f066b060d5
197PM09466 - Nguyễn Trần Khương - K25T-PM <khuong.197pm09466@vanlanguni.vn>
2021-06-03 17:12:07 +0700

    QuanLiDonHang

--- WebApplication1/WebApplication1/Models/Model1.Designer.cs
+++ WebApplication1/WebApplication1/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\tuyen\MON_Computer\code\tuyen\tuyen\github\aendy2000\LoziSneaker\WebApplication1\WebApplication1\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Admin\source\repos\LoziSneaker\WebApplication1\WebApplication1\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
